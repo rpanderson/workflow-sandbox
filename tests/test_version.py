e8e896f864f91c1c343516e3a767566296caf5c9
Russell Anderson <5637107+rpanderson@users.noreply.github.com>
2020-05-04 00:55:32 +1000

    Adding first test to check version attribute

---
+++ tests/test_version.py
@@ -0,0 +1,11 @@
+from pkg_resources import get_distribution
+import workflow_sandbox
+
+
+def test_version():
+    """Check version against `pkg_resources` from `setuptools`.
+    """
+    assert (
+        workflow_sandbox.__version__
+        == get_distribution('workflow_sandbox').version
+    )
