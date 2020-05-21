fdb55e77f701a3b7bd2bc0cbe232bc3f1f71038c
Russell Anderson <5637107+rpanderson@users.noreply.github.com>
2020-05-21 13:45:21 +1000

    Use guess-next-dev version scheme until setuptools_scm release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 VERSION_SCHEME = {}
 VERSION_SCHEME["version_scheme"] = os.environ.get(
-    "SCM_VERSION_SCHEME", "release-branch-semver"
+    "SCM_VERSION_SCHEME", "guess-next-dev"
 )
 VERSION_SCHEME["local_scheme"] = os.environ.get("SCM_LOCAL_SCHEME", "node-and-date")
 
