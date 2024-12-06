49bcf38d5d7c2c23e5813e73edd4af7a26969d86
chrisjbillington <chrisjbillington@gmail.com>
2024-12-06 16:01:55 +1100

    Switch to `pyproject.toml`-only layout
    hard-code release-branch-semver and no-local-version
    
    These are no longer configured to be different in CI than locally
    
    Drop minimum version requirements for build requirements where those
    versions are now several years old

--- workflow_sandbox/__version__.py
+++ workflow_sandbox/__version__.py
@@ -1,27 +1,17 @@
-import os
 from pathlib import Path
-
-try:
-    # Standard library in Python 3.8+
-    from importlib.metadata import version, PackageNotFoundError
-except ImportError:
-    # The backport of the Python 3.8 stdlib module
-    from importlib_metadata import version, PackageNotFoundError
+import importlib.metadata
 
 root = Path(__file__).parent.parent
 if (root / '.git').is_dir():
-    # Use setuptools_scm when in a git repository
     from setuptools_scm import get_version
 
     __version__ = get_version(
         root,
         version_scheme="release-branch-semver",
-        local_scheme=os.getenv("SCM_LOCAL_SCHEME", "node-and-date"),
+        local_scheme="no-local-version",
     )
 else:
-    # Get the version at runtime from PEP-0566 metadata using `importlib.metadata`
-    # from the standard library or the `importlib_metadata` backport
     try:
-        __version__ = version(__package__)
-    except PackageNotFoundError:
+        __version__ = importlib.metadata.version(__package__)
+    except importlib.metadata.PackageNotFoundError:
         __version__ = None
